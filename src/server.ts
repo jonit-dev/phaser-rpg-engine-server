85c4651e89b7765c5809537e62dd888a378092a4
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-01-16 17:31:22 -0800

    rm node modules

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import express from "express";
-
 import { serverRouter } from "./resources/server/server.routes";
 
 const app = express();
@@ -12,4 +11,21 @@ app.use(serverRouter);
 
 const server = app.listen(port, () => {
   console.log(`⚙️ Server running on port ${port}`);
+
+  const geckos = async () => {
+    // import geckos as ESM
+    const { geckos } = await import("@geckos.io/server");
+    const io = geckos();
+
+    io.addServer(server);
+    io.onConnection((channel) => {
+      channel.on("chat message", (data) => {
+        console.log(`got ${data} from "chat message"`);
+        // emit the "chat message" data to all channels in the same room
+        io.room(channel.roomId).emit("chat message", data);
+      });
+    });
+  };
+
+  geckos();
 });
